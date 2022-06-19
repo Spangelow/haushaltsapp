018097ce8522a79f8edb6d4c447a150740955191
Simon Spang <simon.spang@web.de>
2022-06-19 22:56:06 +0200

    aktueller Stand

--- src/js/main.js
+++ src/js/main.js
@@ -19,58 +19,106 @@ const accountBook = {
   enterEntryData() {
     this.entries.push({
       title: prompt("Titel eingeben"),
-      type: prompt("Einnahme oder Ausgabe?"),
-      amount: prompt("Betrag eingeben (Komma als Punkt angeben!)"),
+      type: prompt("Einnahme (E) oder Ausgabe (A)?"),
+      amount: parseFloat(prompt("Betrag eingeben (Komma als Punkt angeben!)")),
       date: prompt("Datum der Ausgabe/Einnahme (jjjj-mm-tt)?"),
+    });
+    this.calculateBalance();
+  },
+
+  // printEntries() {
+  //   this.entries.forEach(element => {
+  //     for (let pair of Object.entries(element)) {
+  //       //console.log(pair);
+  //       let count = 0;
+  //       for (let e of pair) {
+  //         console.log(e);
+  //         if (count%2) {
+  //           console.log("================");
+  //         }
+  //         count++;
+  //       }
+  //     }
+  //   });
+  // },
+  printEntries() {
+    // Falls printEntries() Teil der addNewEntry() ist
+    console.clear();
+    this.entries.forEach((element) => {
+      console.log(
+        `Titel: ${element.title}\nTyp: ${element.type}\nBetrag: ${element.amount}\nDatum: ${element.date}`
+      );
     });
   },
 
-  // printEntry(title, type, amount, date) {
-  //   console.log(
-  //     `Titel: ${title}\nTyp: ${type}\nBetrag: ${amount}\nDatum: ${date}`
-  //   );
-  // },
+  calculateBalance() {
+    let newtotalBalance = {
+      income: 0,
+      expenses: 0,
+      balance: 0,
+    };
 
-  // calculateBalance(type, amount) {
-  //   switch (type) {
-  //     case "Einnahme":
-  //       accountBook.totalBalance.income += amount;
-  //       this.totalBalance.balance += amount;
-  //       break;
-  //     case "Ausgabe":
-  //       this.totalBalance.expenses += amount;
-  //       this.totalBalance.balance -= amount;
-  //       break;
-  //     default:
-  //       console.log(`Der Typ ${type} ist nicht bekannt!`);
-  //       break;
-  //   }
-  // },
+    this.entries.forEach((element) => {
+      switch (element.type) {
+        case "E":
+          newtotalBalance.income += element.amount;
+          break;
+        case "A":
+          newtotalBalance.expenses += element.amount;
+          break;
+        default:
+          console.log(`Der Typ ${element.type} ist nicht bekannt!`);
+          break;
+      }
+    });
+    newtotalBalance.balance = newtotalBalance.income - newtotalBalance.expenses;
+    this.totalBalance = newtotalBalance;
+  },
 
-  // printBalance() {
-  //   console.log("Einnahmen: " + this.totalBalance.income);
-  //   console.log("Ausgaben: " + this.totalBalance.expenses);
-  //   console.log("Bilanz: " + this.totalBalance.balance);
-  //   let balancePositive = this.totalBalance.balance >= 0; // Wandelt in boolean
-  //   console.log(`Positive Bilanz: ${balancePositive}`);
-  // },
+  printBalance() {
+    console.log("============== Bilanz ==============");
+    console.log("Einnahmen: " + this.totalBalance.income);
+    console.log("Ausgaben: " + this.totalBalance.expenses);
+    console.log("Bilanz: " + this.totalBalance.balance);
+    let balancePositive = this.totalBalance.balance >= 0; // Wandelt in boolean
+    if (balancePositive) {
+      console.log(`Die Bilanz ist positiv`);
+    }
+    console.log("===================================");
+  },
+
+  sortEntriesByDate() {
+    console.log("Hallo?");
+    this.entries = this.entries.sort((a,b) =>  {
+      if (a.date > b.date) {
+        // ziehe a weiter nach vorne
+        return -1;
+      } else if (a.date < b.date) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    console.log(this.entries);  
+  },
 
   addNewEntry() {
-    this.enterEntryData();
-    // this.printEntry(
-    //   this.entry.title,
-    //   this.entry.type,
-    //   this.entry.amount,
-    //   this.entry.date
-    // );
-    // this.calculateBalance(this.entry.type, this.entry.amount);
-    // this.printBalance(
-    //   this.totalBalance.income,
-    //   this.totalBalance.expenses,
-    //   this.totalBalance.balance
-    // );
+    let addAnotherEntry = true;
+    while (addAnotherEntry) {
+      this.enterEntryData();
+      this.printEntries();
+      this.calculateBalance();
+      // this.printBalance();
+      addAnotherEntry = confirm("Einen weiteren Eintrag hinzufügen?");
+    }
   },
 };
 
 accountBook.addNewEntry();
-console.log(accountBook);
+
+accountBook.sortEntriesByDate();
+accountBook.printEntries();
+accountBook.calculateBalance();
+accountBook.printBalance();
+
+// console.log(accountBook);
